fcb055d5eafd9d35cf6851f15805ae9967d6eb5f
joegoggin <joegoggin27@gmail.com>
2023-07-28 01:50:09 -0400

    update lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,20 +1,86 @@
-local lauline_status_ok, lauline = pcall(require, "lauline")
-if not lauline_status_ok then
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
     return
 end
 
-lauline.setup {
-  options = {
-    icons_enabled = true,
-    theme = "everforest",
-  },
-  sections = {
-    lualine_a = {
-      {
-        "filename",
-        path = 1,
-      }
-    }
-  }
+local hide_in_width = function()
+    return vim.fn.winwidth(0) > 80
+end
+
+local diagnostics = {
+    "diagnostics",
+    sources = { "nvim_diagnostic" },
+    sections = { "error", "warn" },
+    symbols = { error = " ", warn = " " },
+    colored = false,
+    update_in_insert = false,
+    always_visible = true,
 }
 
+local diff = {
+    "diff",
+    colored = true,
+    symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+    cond = hide_in_width,
+}
+
+local mode = {
+    "mode",
+    fmt = function(str)
+        return "-- " .. str .. " --"
+    end,
+}
+
+local filetype = {
+    "filetype",
+    icons_enabled = true,
+}
+
+local branch = {
+    "branch",
+    icons_enabled = true,
+    icon = "",
+}
+
+local location = {
+    "location",
+    padding = 0,
+}
+
+local lsp_clients = function()
+    local clients = ""
+
+    for _, client in ipairs(vim.lsp.get_active_clients()) do
+        clients = clients .. client.name .. " "
+    end
+    return clients
+end
+
+lualine.setup({
+    options = {
+        icons_enabled = true,
+        theme = "everforest",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+        always_divide_middle = true,
+    },
+    sections = {
+        lualine_a = { branch, diagnostics },
+        lualine_b = { mode },
+        lualine_c = { diff },
+        lualine_x = { filetype },
+        lualine_y = { lsp_clients },
+        lualine_z = { location },
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+    },
+    tabline = {},
+    extensions = {},
+})
